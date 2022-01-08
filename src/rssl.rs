2e0f4dd25854e890ce4aea1f84773f0879171657
vladislav <ve@incsub.com>
2022-01-08 15:47:37 +0100

    Simplify the main struct

--- src/rssl.rs
+++ src/rssl.rs
@@ -1,9 +1,4 @@
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyModifiers};
-use tui::{
-    buffer::Buffer,
-    layout::{Constraint, Direction, Layout, Rect},
-    widgets::Widget,
-};
 
 mod actions;
 mod model;
@@ -54,16 +49,5 @@ impl Rssl {
             };
         }
         false
-    }
-}
-
-impl Widget for &Rssl {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let parts = Layout::default()
-            .direction(Direction::Vertical)
-            .constraints([Constraint::Min(2), Constraint::Length(1)].as_ref())
-            .split(area);
-        view::catalog::output(&self.list, &parts[0]).render(parts[0], buf);
-        view::filter::output(self.list.filter()).render(parts[1], buf);
     }
 }
--- src/rssl/view.rs
+++ src/rssl/view.rs
@@ -5,3 +5,20 @@ use tui::style::Color;
 
 const COLOR_BG: Color = Color::Black;
 const COLOR_FG: Color = Color::Gray;
+
+use super::Rssl;
+use tui::{
+    buffer::Buffer,
+    layout::{Constraint, Direction, Layout, Rect},
+    widgets::Widget,
+};
+impl Widget for &Rssl {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let parts = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Min(2), Constraint::Length(1)].as_ref())
+            .split(area);
+        catalog::output(&self.list, &parts[0]).render(parts[0], buf);
+        filter::output(self.list.filter()).render(parts[1], buf);
+    }
+}
